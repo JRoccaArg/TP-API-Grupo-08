71fe755b90b16d46de843341ccb69b7252074b0c
JRoccaArg <juanroccatic@gmail.com>
2026-09-02 14:19:33 -0300

    Actualicé el modelo de datos y limpie carpetas temporales (lo usé para poder cambiar el MySQL a ServerSQL)

--- zenoirprod/src/main/java/com/uade/tpo/Zenoirprod/controllers/EventosController.java
+++ zenoirprod/src/main/java/com/uade/tpo/Zenoirprod/controllers/EventosController.java
@@ -1,6 +1,5 @@
 package com.uade.tpo.Zenoirprod.controllers;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
